cd76e858ed64742655909944bebf520c968c1df6
Michael Staunton <michael@hilltopassociates.com>
2020-02-28 10:29:06 -0500

    appjs and index html started

---
+++ app.js
@@ -0,0 +1,7 @@
+let canvas=document.getElementById("myCanvas")
+let ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
