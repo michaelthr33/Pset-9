1fa2da8b8a05345973525a8e8464612583e84fbe
M. Staunton <michael@hilltopassociates.com>
2020-02-29 18:19:31 -0600

    removed broken win table added tic tac toe added homepage added brickbreaker -broken brickbreaker link added link made designated folders -linksbroken made one css designated javascript folder undid respective folders -tictactoe link works -biscuitbreaker link broken, reason unknown

--- app.js
+++
@@ -1,8 +0,0 @@
-let canvas=document.getElementById("myCanvas")
-let ctx = canvas.getContext("2d");
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
-//test
---
+++ js/tictactoe.js
@@ -0,0 +1,84 @@
+///////////////////// CONSTANTS /////////////////////////////////////
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+///////////////////// APP STATE (VARIABLES) /////////////////////////
+let board;
+let turn="X"
+let win;
+///////////////////// CACHED ELEMENT REFERENCES /////////////////////
+const squares = Array.from(document.querySelectorAll("#board div"));
+const message = document.querySelector("h2");   // grab the subheader
+///////////////////// EVENT LISTENERS ///////////////////////////////
+window.onload = init;
+document.getElementById("board").onclick = takeTurn;
+document.getElementById("reset-button").onclick = init;
+document.getElementById("start-button").onclick=changeturn
+///////////////////// FUNCTIONS /////////////////////////////////////
+function init() {
+  board = [
+    "", "", "",
+    "", "", "",
+    "", "", ""
+  ];
+
+
+  win = null;
+
+  render();
+}
+function render() {
+  board.forEach(function(mark, index) {
+    squares[index].textContent = mark;
+  });
+
+  message.textContent =
+    win === "T" ? "It's a tie!" : win ? `${win} wins!` : `Turn: ${turn}`;
+    
+}
+
+function changeturn() {
+  if (turn =="X"){turn="O"}
+  else if (turn=="O"){turn="X"}
+  message.textContent="Turn: "+turn+""
+  init()
+}
+
+function takeTurn(e) {
+  if (!win) {
+    let index = squares.findIndex(function(square) {
+      return square === e.target;
+    });
+
+    if (board[index] === "") {
+      board[index] = turn;
+      turn = turn === "X" ? "O" : "X";
+      win = getWinner();
+
+      render();
+    }
+  }
+}
+function getWinner() {
+  let winner = null;
+
+  winningConditions.forEach(function(condition, index) {
+    if (
+      board[condition[0]] &&
+      board[condition[0]] === board[condition[1]] &&
+      board[condition[1]] === board[condition[2]]
+    ) {
+      winner = board[condition[0]];
+    }
+  });
+
+  return winner ? winner : board.includes("") ? null : "T";
+
+}
